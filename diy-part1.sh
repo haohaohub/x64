8ceac5c16a5c94d549c4c04e1b7919c69d2983ce
maxocean <39007967+haohaoget@users.noreply.github.com>
2021-03-06 20:19:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean
+git clone https://github.com/garypang13/luci-app-eqos
+cp -r luci-app-eqos package/lean
