25429db5252d35b7ea3961fec77bf90c823b1b63
maxocean <39007967+haohaoget@users.noreply.github.com>
2021-07-26 18:27:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,13 +14,26 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull origin master
+git branch --set-upstream-to=origin/master master
+
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 cp -r luci-theme-argon package/lean
+
 git clone https://github.com/garypang13/luci-app-eqos
 cp -r luci-app-eqos package/lean
+
 # svn co https://github.com/Lienol/openwrt-luci/branches/18.06/themes
 svn co https://github.com/Lienol/openwrt-luci/trunk
 cp -r trunk/themes/luci-theme-argon-dark-mod package/lean
